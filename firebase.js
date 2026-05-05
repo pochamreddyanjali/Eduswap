d7f6d99d0a000a3638c18bfae2064bed4fd0a6ed
pochamreddyanjali <pochamreddyanjali2005@gmail.com>
2026-05-05 20:07:03 +0530

    Initial commit - EduSwap

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8KJUDCxCt9u11xwVJAH_KkiQsVo9ojlQ",
+  authDomain: "eduswap-494fe.firebaseapp.com",
+  projectId: "eduswap-494fe",
+  storageBucket: "eduswap-494fe.firebasestorage.app",
+  messagingSenderId: "485491741775",
+  appId: "1:485491741775:web:99686afbda0b44e1f9063a"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// export auth so other files can use it
+export { auth };
